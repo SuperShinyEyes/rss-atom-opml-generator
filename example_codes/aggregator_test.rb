83ec38275c206eac4363d427b48029c75bc4c81a
Young Park <i.make.seyoung@gmail.com>
2015-12-11 21:37:26 +0200

    Write test_type with RSS and Unknown
    I couldn't find any atom feeds

--- example_codes/aggregator_test.rb
+++ example_codes/aggregator_test.rb
@@ -6,11 +6,18 @@ class TestAggregator < Minitest::Test
     def setup
         @feed_hs = Feed.new("http://www.hs.fi/rss/?osastot=kotimaa")
         @feed_miehikkala = Feed.new("http://www.miehikkala.fi/d5webmiehi/cgi/DREQUEST.PHP?page=rss/meetingitems&show=30")
+        @feed_type_unknown = Feed.new("http://cjc.jeunes-chercheurs.org/syndication/cneser.php")
 
     end
 
     def test_type
-        assert_equal("rss", @feed_hs.type)
+        assert_equal("application/rss+xml", @feed_hs.type)
+        assert_equal("application/rss+xml", @feed_miehikkala.type)
+        assert_equal("Unknown", @feed_type_unknown.type)
     end
+
+    # def test_opml_header
+    #     assert_equal()
+    # end
 
 end
