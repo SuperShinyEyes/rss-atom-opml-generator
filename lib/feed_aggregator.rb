da90b97a031b5fd3a86cece93e95a0442f6ad174
Young Park <i.make.seyoung@gmail.com>
2015-12-12 03:37:21 +0200

    Organize script & dir names

--- lib/feed_aggregator.rb
+++ lib/feed_aggregator.rb
@@ -85,12 +85,35 @@ class Feed
     end
 
     def write_opml(save_path="")
-        content = get_opml_header + @entries + "\n</opml>"
+        content = get_opml_header + @entry_body + "\n</opml>"
         file_name = "#{save_path}#{@feed.description} - #{@time}.opml"
         File.write(file_name, content)
     end
 
     def summary
-        puts @url, @feed, @type, @entries
+        puts @url, @feed, @type, @entry_body
+    end
+end
+
+
+def aggregate(file_name, save_path="")
+    # Split a text file by lines without newline characters
+    urls = File.readlines(file_name).each {|l| l.chomp!}
+    error = 0
+    total = 0
+
+    urls.each do |url|
+        begin
+            total += 1
+            feed = Feedjira::Feed.fetch_and_parse(url)
+        rescue Feedjira::NoParserAvailable, NoMethodError => e
+            error += 1
+            puts "(#{error}/#{total}) Error: Feedjira couldn't parse - #{e}\n\t#{url}"
+        else
+            f=Feed.new(url, feed)
+            f.write_opml save_path
+            puts "(#{total-error}/#{total}) Successfully read #{url}"
+        ensure
+        end
     end
 end
---
+++ lib/main.rb
@@ -0,0 +1,3 @@
+require_relative "aggregator"
+
+aggregate("dummy_urls.txt", "../opml/")
